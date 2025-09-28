2b00a14c485408d86332eb9116081273c6a89d86
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-09-28 15:32:48 +0900

    feat: python stt 라이브러리 import 추가

--- python-client/python_client/__main__.py
+++ python-client/python_client/__main__.py
@@ -1,5 +1,14 @@
+import argparse
+from faster_whisper import WhisperModel
+import sounddevice
+import webrtcvad
+
 def main():
-    print("hello from notely-stt")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=12765)
+    args = parser.parse_args()
+
+    print("hello from notely-stt", args.port)
 
 if __name__ == "__main__":
     main()
